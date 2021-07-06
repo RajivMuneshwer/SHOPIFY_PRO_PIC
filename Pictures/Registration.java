1d2bd1c0830179b4fc45b6a43b475a8ce0f9af21
RajivMuneshwer <65364776+RajivMuneshwer@users.noreply.github.com>
2021-07-06 11:23:48 -0400

    added tags and writes txt file

--- Pictures/Registration.java
+++ Pictures/Registration.java
@@ -3,15 +3,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.SQLOutput;
 import java.time.LocalDate;
-import java.nio.file.Files;
 
 
 class MyFrame
@@ -19,51 +21,64 @@ class MyFrame
         implements ActionListener {
 
     // Components of the Form
-    private Container c;
-    private JLabel title;
+
+    private final Container c;
+    private final JLabel title;
+    private final JButton back;
     public JTextField tname;
-    private JTextArea tout;
-    private JButton sub;
-    private JButton next;
-    private JButton fin;
-    private JButton back;
+    private final JLabel tags;
+    private final JButton sub;
+    private final JButton next;
+    private final JButton fin;
     private JLabel picLabel;
+    private final JComboBox mgroup;
     public String filePath;
-    public String bcode;
+    public String bCode;
+    public String dicTion;
     public int  position;
-    public int list_length;
+    public int listLength;
     public volatile boolean cont;
     public volatile boolean nextGate;
     public volatile boolean backGate;
     public volatile boolean chngNme;
 
+    private final String[] Party = {"Birthday", "Wedding", "Bridal", "Shower", "Baby", "Christmas", "Old Years", "Graduation"};
+
+    private final String[] Enter_Product_Group = {};
+
+    private final String[] majorGroupsNames = {"Enter Product Group", "Party"};
+
+    private final String[][] majorGroups = {Enter_Product_Group,Party};
+
+    private final JCheckBox[] options = {null, null, null, null, null, null, null, null, null};
 
     // constructor, to initialize the components
     // with default values.
-    public MyFrame(String path, String currentName, int index, int length)
+    public MyFrame(String path, String currentName, int index, int length, String dictioNary)
     {   filePath = path;
         position = index;
         cont = false;
         chngNme = false;
-        list_length = length -1;
-        bcode = currentName;
+        listLength = length -1;
+        bCode = currentName;
+        dicTion = dictioNary;
+        int chkWidth = 30;
+        int chkLength = 200;
+        int chkYPos = 130;
 
-
-        if (index < list_length) {
+        if (index < listLength) {
             nextGate = true;
-        }
-        else if (index == list_length){
+        } else if (index == listLength){
             nextGate = false;
         }
 
         if (index == 0) {
             backGate = false;
-        }
-        else if (index > 0){
+        } else if (index > 0) {
             backGate = true;
         }
 
-        setTitle(bcode);
+        setTitle(bCode);
         setBounds(500, 90, 700, 400);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setResizable(false);
@@ -84,10 +99,17 @@ class MyFrame
         c.add(tname);
         String code = tname.getText();
 
+        tags = new JLabel("Tags");
+        tags.setFont(new Font("Arial", Font.PLAIN, 15));
+        tags.setSize(50,20);
+        tags.setLocation(408,115);
+        tags.setVisible(false);
+        c.add(tags);
+
         sub = new JButton("Submit");
         sub.setFont(new Font("Arial", Font.PLAIN, 15));
         sub.setSize(70, 20);
-        sub.setLocation(407, 86);
+        sub.setLocation(407, 320);
         sub.addActionListener(this);
         c.add(sub);
 
@@ -96,7 +118,7 @@ class MyFrame
         back.setSize(70, 20);
         back.setLocation(408, 350);
         back.addActionListener(this);
-        if (backGate == false){
+        if (!backGate){
             back.setEnabled(false);
         } else{
             back.setEnabled(true);
@@ -115,13 +137,55 @@ class MyFrame
         next.setSize(70, 20);
         next.setLocation(550, 350);
         next.addActionListener(this);
-        if (nextGate == false){
+        if (!nextGate){
             next.setEnabled(false);
         } else{
             next.setEnabled(true);
         }
-
         c.add(next);
+
+        mgroup = new JComboBox(majorGroupsNames);
+        mgroup.setFont(new Font("Arial", Font.PLAIN, 15));
+        mgroup.setSize(270, 20);
+        mgroup.setLocation(405, 86);
+
+        for (int i =0 ;i < options.length; i++){
+            options[i] = new JCheckBox();
+            options[i].setFont(new Font("Arial", Font.PLAIN, 15));
+            options[i].setSize(chkLength,chkWidth);
+            options[i].setLocation(408, chkYPos + (20 * i));
+            options[i].setEnabled(false);
+            options[i].setVisible(false);
+            c.add(options[i]);
+        }
+
+        mgroup.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getItem().equals(majorGroupsNames[0])) {
+                    tags.setVisible(false);
+                    for (JCheckBox option : options) {
+                        option.setEnabled(false);
+                        option.setVisible(false);
+                        option.setText("");
+                    }
+                } else {
+                    tags.setVisible(true);
+                    for (int j = 1; j < majorGroups.length; j++) {
+                        if (e.getItem().equals(majorGroupsNames[j])) {
+                            for (int i = 0; i < majorGroups[j].length; i++) {
+                                options[i].setEnabled(true);
+                                options[i].setVisible(true);
+                                options[i].setText(majorGroups[j][i]);
+                            }
+
+                        }
+
+                    }
+                }
+            }
+        });
+        c.add(mgroup);
 
         Image myPicture = null;
         BufferedImage myImage = null;
@@ -135,7 +199,7 @@ class MyFrame
             picLabel.setSize(width,height);
             picLabel.setLocation(0,0);
             c.add(picLabel);
-        } catch (IOException e) {
+        } catch (IOException ignored) {
         }
         setVisible(true);
     }
@@ -145,30 +209,42 @@ class MyFrame
     // by the user and act accordingly
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == sub) {
-            if (tname.getText().replaceAll("\\s","").equals("")) {
+            if (tname.getText().replaceAll("\\s","").equals("") || mgroup.getSelectedItem() == majorGroupsNames[0]) {
                 String def = "";
                 tname.setText(def);
             } else {
-                bcode = tname.getText();
-                Path path = Paths.get(filePath);
+                bCode = tname.getText();
                 Path p = Path.of(filePath);
                 try {
-                    Files.move(p, p.resolveSibling(bcode + ".jpg"));
+                    Files.move(p, p.resolveSibling(bCode + ".jpg"));
                 } catch (IOException ioException) {
                     ioException.printStackTrace();
                 }
                 cont = true;
                 chngNme = true;
-                dispose();
+                dicTion += "'"+ bCode +"'"+ ":" + "'";
 
+                for (int i =0; i < majorGroups.length; i++) {
+                    if (mgroup.getSelectedItem() == majorGroupsNames[i]) {
+                        dicTion += majorGroupsNames[i] + ",";
+                        for (int j = 0; j < majorGroups[i].length; j++){
+                            if (options[j].isSelected()) {
+                                dicTion += majorGroups[i][j] + ",";
+                            }
+                        }
+                        dicTion += "',";
+                    }
+                }
+                dispose();
             }
         }
 
         else if (e.getSource() == next) {
-            if (position < list_length) {
+            if (position < listLength) {
                 position += 1;
-                dispose();
+                dicTion += " ";
                 cont = true;
+                dispose();
             }
         }
 
@@ -179,7 +255,9 @@ class MyFrame
         }
 
         else if (e.getSource() == fin) {
-            System.exit(0);
+            position = -1;
+            cont = true;
+            dispose();
         }
     }
 
@@ -187,30 +265,41 @@ class MyFrame
         return position;}
 
     public String getBarcode(){
-        return bcode;}
+        return bCode;}
 
 }
 
 public class Registration {
-    public static void main(String[] args) throws IOException, InterruptedException
+
+    public static void main(String[] args) throws InterruptedException
     {   String[] pathnames = fileName();
-        String src = path(date()).toString();
+        StringBuilder src = new StringBuilder(path(date()).toString());
         int index = 0;
+        String dictioNary = "";
+        int terminator = 0;
 
-        while (index < (pathnames.length -1)|| index > 0){
-            src += "/" + pathnames[index];
-            MyFrame f = new MyFrame(src, pathnames[index], index, pathnames.length);
-            while(f.cont == false){
-                Thread.sleep(50);
-                continue;
+
+        while (index != -1){
+            src.append("/").append(pathnames[index]);
+            MyFrame f = new MyFrame(src.toString(), pathnames[index], index, pathnames.length, dictioNary);
+            while(!f.cont){
+                Thread.sleep(60);
+                ++ terminator;
+                if (terminator > 10000){
+                    System.exit(0);
+                }
             }
-            if (f.chngNme == true) {
+            terminator = 0;
+            if (f.chngNme) {
                 pathnames[index] = f.getBarcode() + ".jpg";
+                dictioNary = f.dicTion + " ";
             }
-            src = path(date()).toString();
+            src = new StringBuilder(path(date()).toString());
             index = f.getPosition();
-
         }
+        dictioNary = "{" + dictioNary + "}";
+        write(dictioNary);
+        System.exit(0);
     }
 
 
@@ -234,6 +323,15 @@ public class Registration {
     public static LocalDate date(){
         return java.time.LocalDate.now();
     }
+    public static void write(String dictionary)
+    {
+        try {
+            String filename = "../Product list/" + date() + ".txt";
+            FileWriter fw = new FileWriter(filename, true); //the true will append the new data
+            fw.write( dictionary + System.lineSeparator());//appends the string to the file
+            fw.close();
+        } catch (IOException ioe) {
+            System.err.println("IOException: " + ioe.getMessage());
+        }
+    }
 }
-///time to wait((30*60000)) the System.exit(0)
-/// instead just make it update the tout field so it confirms the update went through
